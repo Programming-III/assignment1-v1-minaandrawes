7287c3b5111844f642e09777e15b3ed2c8053f5e
minaandrawes <mina.andrawes@student.giu-uni.de>
2025-11-12 15:51:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,88 @@
 #include <iostream>
 using namespace std;
 
+class animal(
+public:
+   animal.h::animal(){
+    name = "";
+    age = 0;
+    isHungry == false;
+}
+   animal.h::animal(string n, int a, bool isHungry){
+    name = n;
+    age = a;
+    isHungry == false;
+   }
+   ~animal();
+   string getName();
+   int getAge();
+   bool getIsHungry();
+   void setName(string n) ;
+   void setAge(int a) ;
+   void setIsHungry(bool h);
+   void display();
+   void feed();
+   );
+   
+   
+   
+   class mammal: public animal(
+public:
+   bird();
+   bird(string fc);
+   ~bird();
+   string getFurColor();
+   void setFurColor(string f);
+   );
+   
+   
+   class bird : public animal(
+public:
+   bird();
+   bird(float ws);
+   ~bird();
+   float getWingSpan();
+   void setWingSpan(float w);
+   );
+   
+   
+   class reptile: public animal(
+public:
+   reptile();
+   reptile(bool v);
+   ~reptile();
+   bool getIsVenom();
+   void setIsVenom(bool iv);
+   );
+   
+   
+   class enclosure(
+public:
+   enclosure();
+   enclosure(animal* a, int c, int cc);
+   ~enclosure();
+   int getCapacity();
+   int getCurrentCount();
+   void setCapacity(int cap);
+   void setCurrentCount(int count);
+   void addAnimal(animal* a);
+   void displayAnimals();
+);
+
+
+class visitor(
+public:
+   visitor();
+   visitor(string vn, int tb);
+   ~visitor();
+   string getVisitorName();
+   int getTicketsBought();
+   void setVistorName(string n);
+   void setTicketsBought(int t);
+   void displayInfo();
+   
+   );
+
 
 // ============== MAIN FUNCTION ==============
 int main() {
